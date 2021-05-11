0b2df21a7b9a77879bedc87c385803dbb2ab8f3e
Alex Standiford <a@alexstandiford.com>
2021-05-11 11:35:40 -0700

    Corrects underpin autoloader

--- logger.php
+++ logger.php
@@ -12,6 +12,7 @@ add_action( 'underpin/before_setup', function ( $file, $class ) {
 		require_once( plugin_dir_path( __FILE__ ) . 'lib/loaders/Logger.php' );
 		require_once( plugin_dir_path( __FILE__ ) . 'lib/abstracts/Event_Type.php' );
 		require_once( plugin_dir_path( __FILE__ ) . 'lib/factories/Event_Type_Instance.php' );
+		require_once( plugin_dir_path( __FILE__ ) . 'lib/cron-jobs/Purge_Logs.php' );
 		require_once( plugin_dir_path( __FILE__ ) . 'lib/abstracts/Writer.php' );
 		require_once( plugin_dir_path( __FILE__ ) . 'lib/factories/Basic_Logger.php' );
 		require_once( plugin_dir_path( __FILE__ ) . 'lib/factories/Log_Item.php' );
@@ -24,7 +25,7 @@ add_action( 'underpin/before_setup', function ( $file, $class ) {
 		] );
 
 		// Setup Cron jobs
-		Underpin\underpin()->get( $file, $class )->cron_jobs()->add( 'purge_logs', 'Underpin\Cron_Jobs\Purge_Logs' );
+		Underpin\underpin()->get( $file, $class )->cron_jobs()->add( 'purge_logs', 'Underpin_Logger\Cron_Jobs\Purge_Logs' );
 
 		// Setup Decision Lists
 		Underpin\underpin()->get( $file, $class )->decision_lists()->add( 'event_type_purge_frequency', 'Underpin_Logger\Decisions\Event_Type_Purge_Frequency\Event_Type_Purge_Frequency' );
